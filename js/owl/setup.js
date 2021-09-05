8c3261de1a9b9149de6724132b67e42539ee2ad0
Lorrane Alves <Lorrane.dev@gmail.com>
2021-09-05 18:30:35 -0300

    Adicionado arrows e ajustado layouts do carrosel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -2,7 +2,9 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    nav: true,
+    dots: false,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     responsive:{
         0:{
             items:1
